68c27fd590ece4ac8461447d918e4289eb50b6c0
Yra-A <1325083019@qq.com>
2023-08-09 23:37:42 +0800

    further improve the framework. add some global constants. begin to implement user mode.

---
+++ cmd/api/rpc/init.go
@@ -0,0 +1,6 @@
+package rpc
+
+// 初始化 rpc 客户端
+func InitRPC() {
+  initUserRpc()
+}
---
+++ cmd/api/rpc/user.go
@@ -0,0 +1,68 @@
+// Copyright 2021 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package rpc
+
+import (
+  "context"
+  "github.com/Yra-A/Douyin_Simple_Demo/pkg/middleware"
+  "time"
+
+  "github.com/Yra-A/Douyin_Simple_Demo/kitex_gen/user"
+  "github.com/Yra-A/Douyin_Simple_Demo/kitex_gen/user/userservice"
+  "github.com/Yra-A/Douyin_Simple_Demo/pkg/constants"
+  "github.com/Yra-A/Douyin_Simple_Demo/pkg/errno"
+  "github.com/cloudwego/kitex/client"
+  "github.com/cloudwego/kitex/pkg/retry"
+  etcd "github.com/kitex-contrib/registry-etcd"
+  trace "github.com/kitex-contrib/tracer-opentracing"
+)
+
+var userClient userservice.Client
+
+func initUserRpc() {
+  r, err := etcd.NewEtcdResolver([]string{constants.EtcdAddress})
+  if err != nil {
+    panic(err)
+  }
+
+  c, err := userservice.NewClient(
+    constants.UserServiceName,
+    client.WithMiddleware(middleware.CommonMiddleware),
+    client.WithInstanceMW(middleware.ClientMiddleware),
+    client.WithMuxConnection(1),                       // mux
+    client.WithRPCTimeout(3*time.Second),              // rpc timeout
+    client.WithConnectTimeout(50*time.Millisecond),    // conn timeout
+    client.WithFailureRetry(retry.NewFailurePolicy()), // retry
+    client.WithSuite(trace.NewDefaultClientSuite()),   // tracer
+    client.WithResolver(r),                            // resolver
+  )
+  if err != nil {
+    panic(err)
+  }
+  userClient = c
+}
+
+// UserRegister 用户注册 rpc 客户端
+func UserRegister(ctx context.Context, req *user.UserRegisterRequest) error {
+  resp, err := userClient.UserRegister(ctx, req)
+  if err != nil {
+    return err
+  }
+  if resp.StatusCode != 0 {
+    return errno.NewErrNo(resp.StatusCode, *resp.StatusMsg)
+  }
+  return nil
+}
