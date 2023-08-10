aaa3919f9722343ed573d37f0af99f554e6c9068
Yra-A <1325083019@qq.com>
2023-08-10 10:29:54 +0800

    delete api.thrift

--- idl/api.thrift
+++
@@ -1,9 +0,0 @@
-namespace go api
-
-include "comment.thrift"
-include "favorite.thrift"
-include "feed.thrift"
-include "message.thrift"
-include "publish.thrift"
-include "relation.thrift"
-include "user.thrift"
